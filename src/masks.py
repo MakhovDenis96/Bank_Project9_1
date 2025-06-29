924fc4a59322e3ba65acc4db20ca4da87963b181
Makhov Denis <grehkino@gmail.com>
2025-06-29 11:13:15 +0300

    pyproject.toml

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+def get_mask_card_number(number_card: str) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    number_card_1 = "".join(number_card)
+    number_card_1 = number_card_1.replace(number_card_1[6:-4], "******")
+    mask_card = " ".join(number_card_1[i:i+4] for i in range(0, len(number_card_1), 4))
+    return mask_card
+
+
+print(get_mask_card_number("7000792289606361"))
+
+
+def get_mask_account(number_account: str) -> str:
+    """Функция маскировки номера банковского счета"""
+    number_account = number_account.replace(" ", "")
+    mask_account = str(number_account[-4:])
+    return f"**{mask_account}"
+
+
+print(get_mask_account("4394 8530"))
