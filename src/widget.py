f093262f2d00933d2a526e581aba08d14b4df37e
Makhov Denis <grehkino@gmail.com>
2025-06-29 11:15:28 +0300

    src/widget.py

---
+++ src/widget.py
@@ -0,0 +1,10 @@
+from masks import get_mask_card_number
+
+
+def mask_account_card(account_info: str) -> str:
+    """Функция для маскировки номера карты или счета."""
+    pass
+
+
+def get_date():
+    pass
